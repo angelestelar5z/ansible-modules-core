angelestelar5z/ansible-modules-core

commit e977b76f50595af2f9f483803767379a3d66fd6a

Support for EC2 dedicated tenancy option

cloud/amazon/ec2.py
@@ -67,6 +67,12 @@ options:
     required: true
     default: null
     aliases: []
+  tenancy:
+    description:
+      - An instance with a tenancy of "dedicated" runs on single-tenant hardware and can only be launched into a VPC. Valid values are:"default" or "dedicated". NOTE: To use dedicated tenancy you MUST specify a vpc_subnet_id as well. Dedicated tenancy is not available for EC2 "micro" instances. 
+    required: false
+    default: default
+    aliases: []
   spot_price:
     version_added: "1.5"
     description:
@@ -311,6 +317,18 @@ local_action:
     wait: yes
     vpc_subnet_id: subnet-29e63245
     assign_public_ip: yes
+
+# Dedicated tenancy example
+- local_action:
+    module: ec2
+    assign_public_ip: yes
+    group_id: sg-1dc53f72
+    key_name: mykey
+    image: ami-6e649707
+    instance_type: m1.small
+    tenancy: dedicated
+    vpc_subnet_id: subnet-29e63245
+    wait: yes
 
 # Spot instance example
 - local_action:
@@ -728,6 +746,7 @@ def create_instances(module, ec2, override_count=None):
     group_id = module.params.get('group_id')
     zone = module.params.get('zone')
     instance_type = module.params.get('instance_type')
+    tenancy = module.params.get('tenancy')
     spot_price = module.params.get('spot_price')
     image = module.params.get('image')
     if override_count:
@@ -811,6 +830,9 @@ def create_instances(module, ec2, override_count=None):
 
             if ebs_optimized:
               params['ebs_optimized'] = ebs_optimized
+              
+            if tenancy:
+              params['tenancy'] = tenancy
 
             if boto_supports_profile_name_arg(ec2):
                 params['instance_profile_name'] = instance_profile_name
@@ -1153,6 +1175,7 @@ def main():
             count_tag = dict(),
             volumes = dict(type='list'),
             ebs_optimized = dict(type='bool', default=False),
+            tenancy = dict(default='default'),
         )
     )
 

commit 3ec62deb7040c81a5adafb0de16d611a7bc647d2

Don't use 'tenancy' in ec2 spot requests
The problem was introduced in commit e977b76f. 'tenancy' is a parameter of
ec2.run_instances, but not in ec2.request_spot_instances. So it was breaking
the support for spot requests.

cloud/amazon/ec2.py
@@ -834,7 +834,8 @@ def create_instances(module, ec2, override_count=None):
             if ebs_optimized:
               params['ebs_optimized'] = ebs_optimized
               
-            if tenancy:
+            # 'tenancy' always has a default value, but it is not a valid parameter for spot instance resquest
+            if not spot_price:
               params['tenancy'] = tenancy
 
             if boto_supports_profile_name_arg(ec2):