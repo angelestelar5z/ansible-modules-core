angelestelar5z/ansible-modules-core

commit 04b32383625c3a7a1c3e561f543f1f055b6447f1

Sort subnets by the order they are specified in the play
Fixes #172

cloud/amazon/ec2_vpc.py
@@ -506,6 +506,15 @@ def create_vpc(module, vpc_conn):
             'id': sn.id,
         })
 
+    # Sort subnets by the order they were listed in the play
+    order = {}
+    for idx, val in enumerate(subnets):
+        order[val['cidr']] = idx
+
+    # Number of subnets in the play
+    subnets_in_play = len(subnets)
+    returned_subnets.sort(key=lambda x: order.get(x['cidr'], subnets_in_play))
+
     return (vpc_dict, created_vpc_id, returned_subnets, changed)
 
 def terminate_vpc(module, vpc_conn, vpc_id=None, cidr=None):

commit c06fe3a714b339610467733a436df7da254c6cd7

Fixes regression introduced by 04b32383625c3a7a1c3e561f543f1f055b6447f1

cloud/amazon/ec2_vpc.py
@@ -486,14 +486,15 @@ def create_vpc(module, vpc_conn):
             'id': sn.id,
         })
 
-    # Sort subnets by the order they were listed in the play
-    order = {}
-    for idx, val in enumerate(subnets):
-        order[val['cidr']] = idx
+    if subnets is not None:
+        # Sort subnets by the order they were listed in the play
+        order = {}
+        for idx, val in enumerate(subnets):
+            order[val['cidr']] = idx
 
-    # Number of subnets in the play
-    subnets_in_play = len(subnets)
-    returned_subnets.sort(key=lambda x: order.get(x['cidr'], subnets_in_play))
+        # Number of subnets in the play
+        subnets_in_play = len(subnets)
+        returned_subnets.sort(key=lambda x: order.get(x['cidr'], subnets_in_play))
 
     return (vpc_dict, created_vpc_id, returned_subnets, changed)
 