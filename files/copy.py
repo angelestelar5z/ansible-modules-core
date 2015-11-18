angelestelar5z/ansible-modules-core

commit 361810b60b9868f6f46391fcfc644f0ba2eb78c0

fixed remote_src support, now actually copies and does not move

files/copy.py
@@ -19,7 +19,7 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import time
+import tempfile
 
 DOCUMENTATION = '''
 ---
@@ -214,7 +214,8 @@ def main():
             backup            = dict(default=False, type='bool'),
             force             = dict(default=True, aliases=['thirsty'], type='bool'),
             validate          = dict(required=False, type='str'),
-            directory_mode    = dict(required=False)
+            directory_mode    = dict(required=False),
+            remote_src        = dict(required=False, type='bool'),
         ),
         add_file_common_args=True,
         supports_check_mode=True,
@@ -228,6 +229,7 @@ def main():
     validate = module.params.get('validate',None)
     follow = module.params['follow']
     mode   = module.params['mode']
+    remote_src = module.params['remote_src']
 
     if not os.path.exists(src):
         module.fail_json(msg="Source %s failed to transfer" % (src))
@@ -307,7 +309,12 @@ def main():
                 (rc,out,err) = module.run_command(validate % src)
                 if rc != 0:
                     module.fail_json(msg="failed to validate: rc:%s error:%s" % (rc,err))
-            module.atomic_move(src, dest)
+            if remote_src:
+                tmpdest = tempfile.mkstemp(dir=os.basedir(dest))
+                shutil.copy2(src, tmpdest)
+                module.atomic_move(tmpdest, dest)
+            else:
+                module.atomic_move(src, dest)
         except IOError:
             module.fail_json(msg="failed to copy: %s to %s" % (src, dest))
         changed = True

commit 4989c8500e8091e43d60f970f0d6827b21984290

This fixes copy with the remote_src option
It was broken in 361810b60b9 when the remote_src was added. Need to
pass the absolute path to copy2 instead of a tuple.

files/copy.py
@@ -310,7 +310,7 @@ def main():
                 if rc != 0:
                     module.fail_json(msg="failed to validate: rc:%s error:%s" % (rc,err))
             if remote_src:
-                tmpdest = tempfile.mkstemp(dir=os.basedir(dest))
+                _, tmpdest = tempfile.mkstemp(dir=os.path.dirname(dest))
                 shutil.copy2(src, tmpdest)
                 module.atomic_move(tmpdest, dest)
             else: